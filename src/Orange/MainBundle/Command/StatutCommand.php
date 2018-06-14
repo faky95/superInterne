d917e7e9c6e73d6c0b8b9a3cb52742802024edf3
Madiagne SYLLA <madisylla@orange.sn>
2018-06-14 11:33:51 +0000

    RAS

--- src/Orange/MainBundle/Command/StatutCommand.php
+++ src/Orange/MainBundle/Command/StatutCommand.php
@@ -21,8 +21,8 @@ class StatutCommand extends BaseCommand
 		$today = $today->format('Y-m-d H:i:s');
 		$connexion = $this->get('database_connection');
 		$statut_row = array(
-						'action_id' => 0, 'statut_id' => 0, 'utilisateur_id' => 0, 'dateStatut' => $today, 'commentaire' => 'Action échue non soldée'
-		);
+				'statut_id' => 0, 'utilisateur_id' => 1, 'dateStatut' => $today, 'commentaire' => 'Action échue non soldée'
+			);
 		$tacheEchue = $em->getRepository('OrangeMainBundle:Tache')->tacheEchue();
 		$actionEchue = $em->getRepository('OrangeMainBundle:Action')->actionEchue();
 		$actionNonEchue = $em->getRepository('OrangeMainBundle:Action')->actionNonEchue();
@@ -30,7 +30,6 @@ class StatutCommand extends BaseCommand
 			if($tache->getEtatCourant() !== Statut::ACTION_ECHUE_NON_SOLDEE) {
 				$statut_row['tache_id'] 	  			= $tache->getId();
 				$statut_row['statut_id'] 	  			= $em->getRepository('OrangeMainBundle:Statut')->findOneByCode(Statut::ACTION_ECHUE_NON_SOLDEE)->getId();
-				$statut_row['utilisateur_id']           = $em->getReference('OrangeMainBundle:Utilisateur', 1);
 				$statut_row['dateStatut'] 				= date('Y-m-d H:i:s');
 				$statut_row['commentaire'] 				= 'Tache échue non soldée';
 				$connexion->insert('tache_has_statut', $statut_row);
@@ -43,7 +42,6 @@ class StatutCommand extends BaseCommand
 			if($action->getEtatCourant() !== Statut::ACTION_ECHUE_NON_SOLDEE && $action->getEtatReel() !== Statut::ACTION_ECHUE_NON_SOLDEE) {
 				$statut_row['action_id'] 	  			= $action->getId();
 				$statut_row['statut_id'] 	  			= $em->getRepository('OrangeMainBundle:Statut')->findOneByCode(Statut::ACTION_ECHUE_NON_SOLDEE)->getId();
-				$statut_row['utilisateur_id']           = $em->getReference('OrangeMainBundle:Utilisateur', 1);
 				$statut_row['dateStatut'] 				= date('Y-m-d H:i:s');
 				$statut_row['commentaire'] 				= 'Action échue non soldée';
 				$connexion->insert('action_has_statut', $statut_row);
@@ -54,7 +52,6 @@ class StatutCommand extends BaseCommand
 			if($action->getEtatCourant() !== Statut::ACTION_NON_ECHUE && $action->getEtatReel() !== Statut::ACTION_NON_ECHUE) {
 				$statut_row['action_id'] 	  			= $action->getId();
 				$statut_row['statut_id'] 	  			= $em->getRepository('OrangeMainBundle:Statut')->findOneByCode(Statut::ACTION_NON_ECHUE)->getId();
-				$statut_row['utilisateur_id']           = $em->getReference('OrangeMainBundle:Utilisateur', 1);
 				$statut_row['dateStatut'] 				= date('Y-m-d H:i:s');
 				$statut_row['commentaire'] 				= 'Action non échue';
 				$connexion->insert('action_has_statut', $statut_row);
